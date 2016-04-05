eafaad8c1ec1f2cab9811f08715cca21992d74ed
sinhvis <sinhvis@gmail.com>
2016-04-05 07:06:33 -0700

    Defined AngularJS app and controller

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,5 +1,22 @@
 /* JavaScript Document */
+// defining an enclosure of an anonymous function
+// So that this particular function in the JavaScript file can exist with other JavaScripts in a page
+// Everything defined here is going to be to declare the Angular application for the HTML
+// Two main elements needed to define Angular application:
+// app variable and controller 
 
-/* Script goes here */
+(function(){
+
+	// declaring the variable app to be the name myCalculator
+	var app = angular.module('myCalculator', []) ;
+
+	// define controller which will be added to app
+	// CalculatorController - name of the controller assigned to main Calculator container in the HTML
+	// $scope - Angular service
+	// $scope allows passing variables between HTML and JavaScript
+	app.controller('CalculatorController', ['$scope', function($scope) {
+
+	}]) ;
+})() ;
 
 
